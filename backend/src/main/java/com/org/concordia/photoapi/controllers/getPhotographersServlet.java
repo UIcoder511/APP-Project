fab4c6b7c518a77d50041799c8fa12f6223608d8
Umang <umangkumarmaheshbhai.patel@mail.concordia.ca>
2022-11-13 17:08:20 -0500

    backend: fixed No photos bug frontend: changed App name - Picturesque

--- backend/src/main/java/com/org/concordia/photoapi/controllers/getPhotographersServlet.java
+++ backend/src/main/java/com/org/concordia/photoapi/controllers/getPhotographersServlet.java
@@ -1,21 +1,19 @@
 package com.org.concordia.photoapi.controllers;
 
-import java.io.IOException;
-import java.util.ArrayList;
-import java.util.List;
-
-import javax.servlet.ServletException;
-import javax.servlet.annotation.WebServlet;
-import javax.servlet.http.HttpServlet;
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
-
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.org.concordia.photoapi.mappers.PhotographerMapper;
 import com.org.concordia.photoapi.mappers.PhotographerMapperImpl;
 import com.org.concordia.photoapi.model.Photo;
 import com.org.concordia.photoapi.model.Photographer;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 
 @WebServlet(
   name = "getPhotographersServlet",
@@ -31,13 +29,13 @@ public class getPhotographersServlet extends HttpServlet {
     throws ServletException, IOException {
     try {
       List<Photographer> photographers = photographerMapper.getListOfPhotographers();
-      List<Photographer> photographerWithPhotoslist = new ArrayList<Photographer>();
-      
       for (Photographer photographer : photographers) {
-			List<Photo> photos = photographerMapper.getListOfPhotosByPhotographerId(photographer.getphotographerId());
-			photographer.setPhotos(photos);
-			photographerWithPhotoslist.add(photographer);
-		}
+        List<Photo> photos = photographerMapper.getListOfPhotosByPhotographerId(
+          photographer.getphotographerId()
+        );
+
+        photographer.setPhotos(photos);
+      }
 
       ObjectMapper mapper = new ObjectMapper();
       String jsonString = mapper.writeValueAsString(photographers);
--- backend/src/main/java/com/org/concordia/photoapi/mappers/PhotographerMapperImpl.java
+++ backend/src/main/java/com/org/concordia/photoapi/mappers/PhotographerMapperImpl.java
@@ -1,68 +1,61 @@
 package com.org.concordia.photoapi.mappers;
 
+import com.org.concordia.photoapi.gateways.PhotographerGateway;
+import com.org.concordia.photoapi.gateways.PhotographerGatewayImpl;
+import com.org.concordia.photoapi.model.Photo;
+import com.org.concordia.photoapi.model.Photographer;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.util.ArrayList;
 import java.util.List;
 
-import com.org.concordia.photoapi.gateways.PhotographerGateway;
-import com.org.concordia.photoapi.gateways.PhotographerGatewayImpl;
-import com.org.concordia.photoapi.model.Photo;
-import com.org.concordia.photoapi.model.Photographer;
-
 public class PhotographerMapperImpl implements PhotographerMapper {
-	
+
   private PhotographerGateway photographerGateway = new PhotographerGatewayImpl();
 
   @Override
   public List<Photo> getListOfPhotosByPhotographerId(int photographerId) {
-    List<Photo> listOfPhotosByPhotographerName = new ArrayList<Photo>();
+    List<Photo> listOfPhotosByPhotographerId = new ArrayList<Photo>();
 
     try {
-    	
-    	ResultSet rs= photographerGateway.getPhotosByPhotographerId(photographerId);
-    	if(rs!=null)
-    	{
-    		while(rs.next())
-    		{
-    			Photo photo = new Photo();
-    	        photo.setPhotoId(rs.getInt("photo_id"));
-    	        photo.setAvgColor(rs.getString("avg_color"));
-    	        photo.setTitle(rs.getString("title"));
-    	        photo.setImageMediumSize(rs.getString("imageMediumSize"));
-    	        photo.setImageLargeSize(rs.getString("imageLargeSize"));
-    	        photo.setImageOrignalSize(rs.getString("imageOrignalSize"));
-    	
-    	        listOfPhotosByPhotographerName.add(photo);
-    		}
-	        
+      ResultSet rs = photographerGateway.getPhotosByPhotographerId(
+        photographerId
+      );
+      if (rs != null) {
+        while (rs.next()) {
+          Photo photo = new Photo();
+          photo.setPhotoId(rs.getInt("photo_id"));
+          photo.setAvgColor(rs.getString("avg_color"));
+          photo.setTitle(rs.getString("title"));
+          photo.setImageMediumSize(rs.getString("imageMediumSize"));
+          photo.setImageLargeSize(rs.getString("imageLargeSize"));
+          photo.setImageOrignalSize(rs.getString("imageOrignalSize"));
+
+          listOfPhotosByPhotographerId.add(photo);
+        }
       }
     } catch (SQLException e) {
       System.out.println(e.getMessage());
     }
 
-    return listOfPhotosByPhotographerName;
+    return listOfPhotosByPhotographerId;
   }
-  
-@Override
-public List<Photographer> getListOfPhotographers() {
-	List<Photographer> listOfPhotographers = new ArrayList<Photographer>();
+
+  @Override
+  public List<Photographer> getListOfPhotographers() {
+    List<Photographer> listOfPhotographers = new ArrayList<Photographer>();
 
     try {
-    	
-    	ResultSet rs= photographerGateway.getPhotographers();
-    			
-    	if(rs!=null)
-    	{
-    		
-    		while(rs.next())
-    		{
-		        Photographer photographer = new Photographer();
-		        photographer.setPhotographerId(rs.getInt("photographer_id"));
-		        photographer.setphotographerName(rs.getString("p_name"));
-		        photographer.setphotographerUrl(rs.getString("p_url"));
-		        
-    		}
+      ResultSet rs = photographerGateway.getPhotographers();
+
+      if (rs != null) {
+        while (rs.next()) {
+          Photographer photographer = new Photographer();
+          photographer.setPhotographerId(rs.getInt("photographer_id"));
+          photographer.setphotographerName(rs.getString("p_name"));
+          photographer.setphotographerUrl(rs.getString("p_url"));
+          listOfPhotographers.add(photographer);
+        }
       }
     } catch (SQLException e) {
       System.out.println(e.getMessage());
